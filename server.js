acc7bb87f0316d2388fe26e9d2efaee815648955
Ajay Mohan <ajay.mohan@intellectdesign.com>
2017-05-03 17:15:15 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+/*app.get('/',function(req,res){
+	res.send("Hello Express!");
+});
+*/
+var middleWare = {
+	requireAuthentication:function(req,res,next){
+		console.log('private route hit');
+		next();
+	},
+	logger:function(req,res,next){
+		console.log('Request: '+new Date().toString()+' ' +req.method+' '+req.originalUrl);
+		next()
+	}
+};
+
+//app.use(middleWare.requireAuthentication);
+app.use(middleWare.logger);
+
+app.get('/about',middleWare.requireAuthentication,function(req,res){
+	res.send("About us");
+});
+
+app.use(express.static(__dirname+"/public"));
+
+app.listen(port,function(){
+	console.log('Express Server Started');
+	console.log('Running on the port: '+port)
+});
